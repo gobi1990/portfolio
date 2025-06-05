ba0ca1b9e5c9604ca74094bd2929603bbf98d068
Gobishankar <55891717+gobi1990@users.noreply.github.com>
2025-06-05 10:46:53 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -68,7 +68,7 @@ export default function Home() {
         />
        </div>
         <h1 className="text-4xl font-bold mb-4 text-dark-primary">Gobishankar Annalingam</h1>
-        <p className="text-xl text-dark-onBackground mb-8">Mobile App Developer | Game Developer | Front-end Developer</p>
+        <p className="text-xl text-dark-onBackground mb-8">Full Stack Developer | Mobile App Developer | Game Developer </p>
         
         <div className="flex flex-col md:flex-row gap-10 justify-center items-center">
         <CustomButton title="Resume" icon={<Download className="w-5 h-5 mr-2"  />} onClick={resumeDownload}/>
